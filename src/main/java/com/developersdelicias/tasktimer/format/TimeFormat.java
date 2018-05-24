ee904f031adcb96d240d856723f1551565baefc8
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2018-05-24 09:08:38 -0600

    Fix #15: Checkstyles of TimeFormat interface

--- src/main/java/com/developersdelicias/tasktimer/format/TimeFormat.java
+++ src/main/java/com/developersdelicias/tasktimer/format/TimeFormat.java
@@ -1,5 +1,15 @@
 package com.developersdelicias.tasktimer.format;
 
+/**
+ * Defines a format for Time elements.
+ */
 public interface TimeFormat {
+
+    /**
+     * Creates a String representation of the current time given the number of
+     * milliseconds elapsed.
+     * @param milliseconds Number of milliseconds
+     * @return A String representation of the time elapsed
+     */
     String format(long milliseconds);
 }
