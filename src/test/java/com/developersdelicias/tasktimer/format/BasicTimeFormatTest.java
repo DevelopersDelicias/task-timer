a18b34d3e5f3ed41c64dfccae7ce0edebf63cf19
Benjamin Cisneros Barraza <cisnerosbarraza@gmail.com>
2024-01-31 16:51:07 -0600

    Update assertThat import

--- src/test/java/com/developersdelicias/tasktimer/format/BasicTimeFormatTest.java
+++ src/test/java/com/developersdelicias/tasktimer/format/BasicTimeFormatTest.java
@@ -1,7 +1,7 @@
 package com.developersdelicias.tasktimer.format;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
 
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
